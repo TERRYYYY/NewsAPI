12bd0d74ff0833eb4ebd8f2ae35f21c689208451
TERRYYYY <kariuki.terry11@gmail.com>
2020-04-27 13:34:36 +0300

    updating news and article models

---
+++ app/models.py
@@ -0,0 +1,19 @@
+class News:
+    def __init__(self , id , name , description , pubishedAt, author, urlToImage, url):
+        
+        self.id = id
+        self.name = name
+        self.description = description
+        self.pubishedAt = pubishedAt
+        self.author = author
+        self.urlToImage = urlToImage
+        self.url = url
+
+
+class Article:
+    def __init__(self,id,title,content,name,url):
+        self.id = id
+        self.title = title
+        self.content = content
+        self.name = name
+        self.url = url
