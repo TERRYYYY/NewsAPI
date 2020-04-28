b332d246bc8f63ce1de42f0a66ac5f76c0ca6272
TERRYYYY <kariuki.terry11@gmail.com>
2020-04-28 11:28:04 +0300

    added a readme file

--- config.py
+++ config.py
@@ -5,10 +5,7 @@ class Config:
     '''
     NEWS_API_BASE_URL = 'https://newsapi.org/v2/everything?q={}&apiKey={}'
     ARTICLE_BASE_URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey={}'
-    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
-    
-
-
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')    
 
 class ProdConfig(Config):
     '''
--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-export MOVIE_API_KEY='12d688c8a85c4864ba3c8dac4ee62038'
+export NEWS_API_KEY='12d688c8a85c4864ba3c8dac4ee62038'
 python3.7 manage.py server
